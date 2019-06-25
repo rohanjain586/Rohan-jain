d070ee87854aedba8d1d687272f6b6e69f35e78d
Rohan kumar jain <Rohan kumar jain@DESKTOP-2QOB21C>
2019-06-25 11:00:35 +0530

    added class Character.java

---
+++ Jain/src/Character.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Character {
+    public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+        char s1,s2;
+        s1=sc.next().charAt(0);
+        s2=sc.next().charAt(0);
+        if(s1<s2)
+            System.out.println(s2+","+s1);
+        else
+         System.out.println(s2+","+s1);   
+        
+    }
+    
+}
