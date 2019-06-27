d737b0a306d34a8a23e62bf2f9debb8eb3e94667
Rohan kumar jain <Rohan kumar jain@192.168.0.5>
2019-06-27 19:56:33 +0530

    added class Month.java

---
+++ Jain/src/Month.java
@@ -0,0 +1,54 @@
+import java.util.*;
+public class Month {
+    public static void main(String []args){
+        int n;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        switch(n){
+            
+        case 1:
+                System.out.println("january");
+                break;
+        case 2:
+                System.out.println("february");
+                break;
+        case 3:
+                System.out.println("march");
+                break;
+        case 4:
+                System.out.println("April");
+                break;
+        case 5:
+                System.out.println("May");
+                break;
+        case 6:
+                System.out.println("june");
+                break;
+        case 7:
+                System.out.println("july");
+                break;
+        case 8:
+                System.out.println("August");
+                break;
+        case 9:
+                System.out.println("September");
+                break;
+        case 10:
+                System.out.println("october");
+                break;
+        case 11:
+                System.out.println("novembr");
+                break;
+        case 12:
+                System.out.println("December");
+                break;
+        
+        default:
+            System.out.println("Invalid month");
+            break;
+                                            
+        }
+    }
+    
+    
+}
